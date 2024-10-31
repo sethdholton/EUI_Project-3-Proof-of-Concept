d262a9a6a815e1d3f60e994b89cc95530e72ed8e
Seth Holton <sethdholton@utexas.edu>
2024-10-31 03:11:47 -0500

    File upload

--- sketch.js
+++ sketch.js
@@ -8,16 +8,29 @@ let startAudio = false;
 let inputMic = false;
 let bass, lowMid, mid, highMid, treble;
 let soundAvg;
-sf = [];
-sfIndex = 0;
+let sf = [];
+let bassEnergy;
+let lastBeatTime = 0;
+let bpm = 120;
+let beatInterval = (60 / bpm) * 1000;
+let freqThreshold = 150;
+
+let camX = 0;
 
 let spectrum, waveform; // spectrum and waveform
 
 let sLen, wLen; // spectrum length and waveform length
 
-let turnSpeed;
+let displaythoughtbubble = false;
 
-let sf1, sf2, sfAsleep; // image assets
+let turnSpeed;
+let displace;
+let tempDisplace = 0;
+
+let spawnInterval = 0;
+let phase = 0;
+
+let sf1, sf2, sfAsleep, thoughtbubble; // image assets
 
 let partyGoers = [];
 
@@ -29,47 +42,72 @@ function preload() {
     for (let i = 0; i < 2; i++) {
       sf[i] = loadImage("/assets/sf/" + i + ".png");
     }
-    
-    // sound = loadSound('/assets/demoserenade.mp3');
-    // sound = loadSound('/assets/smooveguitar.mp3');
+
+    thoughtbubble = loadImage("/assets/thoughtbubble.png");
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     sound.amp(.8);
-    colorMode(HSB);
+    colorMode(RGB);
     textFont("Courier New");
     background(0);
+
+    // background
+    partyGoers.push(new PartyGoer(width*1.3, height/2, 3, false));
+    partyGoers.push(new PartyGoer(width*0.95, height/2, 3, false));
+    partyGoers.push(new PartyGoer(width * 1.5, height/2, 3, false));
+    partyGoers.push(new PartyGoer(width * 2.1, height/2, 3, false));
+    partyGoers.push(new PartyGoer(width * 2.4, height/2, 3, false));
+    
+    // foreground
+    partyGoers.push(new PartyGoer(width*0.5, height/2, 4, false));
+    partyGoers.push(new PartyGoer(width, height/2, 4, false));
+    partyGoers.push(new PartyGoer(width * 1.3, height/2, 4, false));
+    partyGoers.push(new PartyGoer(width * 1.7, height/2, 4, false));
+
+    // spawnNewPartyGoers();
 }
 
 function draw() {
-    background(0);
+    background(200);
+
+    noStroke();
+    fill(100);
+    rect(0, height*0.6, width, height*0.4);
+
+    displace = (camX*-1) + (width);
 
     if (startAudio) {
-      
       updateAudio();
-      updateVisuals();
+      // updateVisuals();
 
-      displayScene();
+      push();
+        translate(camX, 0);
+        displayScene();
+        if (displaythoughtbubble) {
+          strokeWeight(4);
+          stroke(0);
+          fill(255);
+          circle(width*0.3 + tempDisplace, height*0.7, width*0.01);
+          circle(width*0.45 + tempDisplace, height*0.6, width*0.02);
+          circle(width*0.6 + tempDisplace, height*0.55, width*0.05);
+          circle(width*0.9 + tempDisplace, height*0.5, width*0.075);
+          image(thoughtbubble, width*0.9 + tempDisplace, 0, thoughtbubble.width*1.53, thoughtbubble.height*1.53);
+        }
+    
+      pop();
+
+      camX--;
+      if (camX % width == 0) {
+        spawnNewPartyGoers();
+        phase++;
+        console.log("yes");
+      }
     }
 
-    if (stopped) {
-      fill(100, 255, 255);
-      textAlign(CENTER, CENTER);
-      textSize(width/20);
-      text("PRESS 'SPACE' TO START", width/2, height/2);
-    } //else {
-    //   textAlign(LEFT, CENTER);
-    //   textSize(width/100);
-    //   text("Click to Toggle", width * 0.025, height * 0.9);
-    //   if (inputMic) {
-    //     text("Input: Mic", width * 0.025, height * 0.95);
-    //   } else {
-    //     text("Input: Music", width * 0.025, height * 0.95);
-    //   }
-    // }
-
-    image(sf[indexJump])
+    displayUI();
+    spawnInterval++;
 }
 
 function updateAudio() {
@@ -83,81 +121,85 @@ function updateAudio() {
   soundAvg = (bass + lowMid + mid + highMid + treble) / 5;
   turnSpeed = map(soundAvg, 0, 255, .0000000000000001,.000005);
 
-  // for(let i = 0; i < particles.length; i++) {
-  //   particles[i].display();
-  //   if (particles[i].x > width ||
-  //       particles[i].y > height) {
-  //         particles.splice(i, 1);
-  //       }
+  fft.analyze();
+  bassEnergy = fft.getEnergy("bass");
+
+  // console.log("Bass Energy: ", bassEnergy);
+
+  if (bassEnergy > freqThreshold && millis() - lastBeatTime > beatInterval * 0.8) {
+    for(let i = 0; i < partyGoers.length; i++) {
+      partyGoers[i].anim();
+    }
+    lastBeatTime = millis();
+    // console.log("Beat detected!");
+  }
+
+  let timeSinceLastBeat = millis() - lastBeatTime;
+  let speedC = map(timeSinceLastBeat, 0, beatInterval, 0, width);
+
+  // ellipse(speedC, height / 2, height * 0.5, height * 0.5);
+  // if (speedC == 0) {
+  //   for(let i = 0; i < partyGoers.length; i++) {
+  //     partyGoers[i].anim();
+  //   }
   // }
 
-  spectrum = fft.analyze();
-  sLen = Math.floor(spectrum.length / 5);
+  // spectrum = fft.analyze();
+  // sLen = Math.floor(spectrum.length / 5);
 
-  waveform = fft.waveform();
-  wlen = waveform.length;
+  // waveform = fft.waveform();
+  // wlen = waveform.length;
 
-}
-
-function updateVisuals() {
-  if (soundAvg > 160) {
-    partyGoers.push(new PartyGoer());
-  }
 }
 
 function displayScene() {
-  // for (let i = 0; i < sLen; i++) {
-  //   h = map(spectrum[i], 0, 255, 0, height);
-  //   push();
-  //     translate(width/2, height/2);
-  //     let r = map(i, 0, sLen, 0, TWO_PI);
-  //     rotate(r-(millis()*turnspeed));
-  //     let thick = h * 0.005;
-  //     fill(150);
-  //     triangle(-5, height-(h*1.05), 0, height, 0 + thick, height);
-  //     fill(125);
-  //     triangle(0, height - (h*1.), 0, height, 10 + thick, height);
-  //     fill(100);
-  //     triangle(0, height - (h*0.95), 0, height, 15 + thick, height);
-  //     fill(75);
-  //     triangle(0, height - (h*0.9), 0, height, 20 + thick, height);
-  //     fill(0);
-  //     triangle(0, height - (h*0.85), 0, height, 25 + thick, height);
-  //   pop();
-  // }
-  // for ( let i = 0; i < sLen; i++) {
-  //   h = map(spectrum[i], 0, 255, 0, height);
-  //   push();
-  //     translate(width/2, height/2.05);
-  //     rotate(millis()/25000);
-  //     push();
-  //       let r = map(i, 0, sLen, 0, TWO_PI);
-  //       rotate(r+(millis()/15000));
-  //       fill(420-h*0.5, h*0.5, 255);
-  //       noStroke();
-  //       ellipse(0, (h*0.095)+height/10, (h*50)/width, h*0.33);
-  //     pop();
-  //   pop();
-  // }
-
-  // for (let i = 0; i < wlen; i++) {
-  //   let y = map(waveform[i], 1, -1, 0, height/20);
-  //   push();
-  //     translate(width/2, height/2);
-  //     let r = map(i, 0, wlen, 0, TWO_PI);
-  //     rotate(r+millis()/20000);
-  //     stroke(255);
-  //     strokeWeight(1);
-  //     line(0, y, 0, 0);
-  //   pop();
-  // }
-
-  let img = sf[sfIndex];
-  image(img, width/2, height/2, img.width, img.height);
-  sfIndex++;
-  if (sfIndex >= sf.length) {
-    sfIndex = 0;
+  for (let i = 0; i < partyGoers.length; i++) {
+    partyGoers[i].display();
   }
+}
+
+function spawnNewPartyGoers() {
+  if (phase == 0) {
+    // background
+    partyGoers.push(new PartyGoer(width*1.3 + displace, height/2, 3, false));
+    partyGoers.push(new PartyGoer(width*0.95 + displace, height/2, 3, false));
+    // foreground
+    partyGoers.push(new PartyGoer(width*0.5 + displace, height/2, 4, false));
+    partyGoers.push(new PartyGoer(width + displace, height/2, 4, false));
+  } else if (phase == 1) {
+    partyGoers.push(new PartyGoer(width*1.3 + displace, height/2, 3, false));
+    partyGoers.push(new PartyGoer(width*0.95 + displace, height/2, 3, true));
+    // foreground
+    partyGoers.push(new PartyGoer(width*0.5 + displace, height/2, 4, false));
+    partyGoers.push(new PartyGoer(width + displace, height/2, 4, false));
+  } else if (phase == 2) {
+    partyGoers.push(new PartyGoer(width*1.3 + displace, height/2, 3, true));
+    partyGoers.push(new PartyGoer(width*0.95 + displace, height/2, 3, true));
+    // foreground
+    partyGoers.push(new PartyGoer(width*0.5 + displace, height/2, 4, true));
+    partyGoers.push(new PartyGoer(width + displace, height/2, 4, true));
+    displaythoughtbubble = true;
+  } else if (phase == 3) {
+    tempDisplace = displace;
+    phase = 0;
+  }
+}
+
+function displayUI() {
+  fill(100, 255, 100);
+  if (stopped) {
+    textAlign(CENTER, CENTER);
+    textSize(width/20);
+    text("PRESS 'SPACE' TO START", width/2, height/2);
+  } else {
+     textAlign(LEFT, CENTER);
+     textSize(width/100);
+     if (inputMic) {
+       text("Mic", width * 0.025, height * 0.95);
+     } else {
+       text("Music", width * 0.025, height * 0.95);
+     }
+   }
 }
 
 let stopped = true;
@@ -176,43 +218,66 @@ function keyPressed() {
 
     if (stopped) {
         loop();
-        // background(0);
         if (!inputMic) {
           sound.loop();
         }
         stopped = false;
     } else {
         noLoop();
-        // background(0);
         if (!inputMic) {
           sound.stop();
         }
         stopped = true;
+        camX = 0;
     }
   }
 }
 
-function mousePressed() {
-  // background(0);
-  if (!stopped) {
-    if (inputMic) {
-      fft.setInput(sound);
-      inputMic = false;
-      sound.loop();
-    } else {
-      fft.setInput(mic);
-      inputMic = true;
-      sound.stop();
-    }
-  }
-}
+// function mousePressed() {
+//   if (!stopped) {
+//     if (inputMic) {
+//       fft.setInput(sound);
+//       inputMic = false;
+//       sound.loop();
+//     } else {
+//       fft.setInput(mic);
+//       inputMic = true;
+//       sound.stop();
+//     }
+//   }
+// }
 
 class PartyGoer
 {
-  constructor(x, y, scale) {
-    this.x = x;
-    this.y = y;
-    this.scale = scale;
+  constructor(x, y, size, asleep) {
+    this.startX = x;
+    this.startY = y;
+    this.x = this.startX;
+    this.y = this.startY;
+    this.size = size;
+    // this.scale = scale;
+    this.onscreen = true;
+    this.sfIndex = 0;
+    this.asleep = asleep;
   }
   
+  display() {
+    let img;
+    if (!this.asleep) {
+      img = sf[this.sfIndex];
+    } else {
+      img = sfAsleep;
+    }
+    this.w = img.width/(5-this.size);
+    this.h = img.height/(5-this.size);
+    image(img, this.x, this.y, img.width/(5-this.size), 
+          img.height/(5-this.size));
+  }
+
+  anim() {
+    this.sfIndex++;
+    if (this.sfIndex >= sf.length) {
+      this.sfIndex = 0;
+    }
+  }
 }
